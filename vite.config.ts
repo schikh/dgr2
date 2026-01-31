bf3302dd4779b14f2d6e1cd26453622b3d228481
Saïd Chikh <said.chikh@ores.be>
2026-01-31 22:14:21 +0100

    fix base address

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dgr2/',     // <-- include leading & trailing slashes
   plugins: [
     react(),
     VitePWA({
